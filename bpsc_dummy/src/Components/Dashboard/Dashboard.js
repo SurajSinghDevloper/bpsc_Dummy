0929099f1b14ad7268e3f38e9d8423eb08edf8f0
Suraj Kumar <suraj31kumar1999@gmail.com>
2024-01-06 18:28:11 +0530

    ui update

--- bpsc_dummy/src/Components/Dashboard/Dashboard.js
+++ bpsc_dummy/src/Components/Dashboard/Dashboard.js
@@ -5,9 +5,13 @@ import PersonLogo from '../../Assets/person.png';
 const Dashboard = (props) => {
   return (
     <DashLayout>
-      <div className="container mx-auto py-8">
+      <div className="container  p-9 ">
         {/* Page header */}
-        <div>
+        <div style={{
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}>
           <IDCard />
         </div>
 
@@ -31,58 +35,60 @@ const Dashboard = (props) => {
 
 const IDCard = () => {
   return (
-    <div className="max-w-md bg-white rounded-lg flex justify-between p-8">
+    <div className="w-11/12  bg-white rounded-lg flex justify-between ">
       <div>
-        <img className="w-32 h-32 rounded-full mx-auto" src={PersonLogo} alt="Profile" />
+        <img className="w-48 h-32 rounded-full mx-auto" src={PersonLogo} alt="Profile" />
         <div className="text-center mt-4">
           <div className="font-bold text-xl">John Doe</div>
           <p className="text-gray-700 text-base">Username: 123456</p>
           <p className="text-gray-700 text-base">Dob: 10/12/1999</p>
         </div>
       </div>
-      <form className='w-1/2 flex'>
-        <div className='flex flex-col w-full'>
-          <div className="flex items-center mb-4 ">
-            <label className="w-full text-right mr-2">Firstname:</label>
-            <input type="text" value="Suraj Kumar" className="w-full p-2 border rounded-md" />
+      <div className=' container  '>
+        <form className=' w-full flex '>
+          <div className=' w-full'>
+            <div className="flex items-center mb-4 ">
+              <label className="w-1/2 text-right mr-2">Firstname:</label>
+              <input type="text" value="Suraj Kumar" className=" p-2 border rounded-md" />
+            </div>
+            <div className="flex items-center mb-4">
+              <label className="w-1/2 text-right mr-2">Middlename:</label>
+              <input type="text" value="Lastname" className=" p-2 border rounded-md" />
+            </div>
+            <div className="flex items-center mb-4">
+              <label className="w-1/2 text-right mr-2">Lastname:</label>
+              <input type="text" value="Lastname" className="p-2 border rounded-md" />
+            </div>
+            <div className="flex items-center mb-4">
+              <label className="w-1/2 text-right mr-2">E-mail ID:</label>
+              <input type="text" value="Lastname" className="p-2 border rounded-md" />
+            </div>
+            {/* Add more fields similarly */}
           </div>
-          <div className="flex items-center mb-4">
-            <label className="w-full text-right mr-2">Middlename:</label>
-            <input type="text" value="Lastname" className="w-2/3 p-2 border rounded-md" />
+
+          <div className=' '>
+            <div className="flex items-center mb-4 ">
+              <label className="w-full text-right mr-2">Username:</label>
+              <input type="text" value="Suraj Kumar" className=" p-2 border rounded-md" />
+            </div>
+            <div className="flex items-center mb-4">
+              <label className="w-full text-right mr-2">Mobile:</label>
+              <input type="text" value="Lastname" className=" p-2 border rounded-md" />
+            </div>
+            <div className="flex items-center mb-4">
+              <label className="w-full text-right mr-2">Mother :</label>
+              <input type="text" value="Lastname" className=" p-2 border rounded-md" />
+            </div>
+            <div className="flex items-center mb-4">
+              <label className="w-full text-right mr-2">Father:</label>
+              <input type="text" value="Lastname" className=" p-2 border rounded-md" />
+            </div>
+            {/* Add more fields similarly */}
           </div>
-          <div className="flex items-center mb-4">
-            <label className="w-full text-right mr-2">Lastname:</label>
-            <input type="text" value="Lastname" className="w-full p-2 border rounded-md" />
-          </div>
-          <div className="flex items-center mb-4">
-            <label className="w-full text-right mr-2">E-mail ID:</label>
-            <input type="text" value="Lastname" className="w-2/3 p-2 border rounded-md" />
-          </div>
-          {/* Add more fields similarly */}
-        </div>
-        <div className='flex flex-col w-1/2 ml-72'>
-          <div className="flex items-center mb-4 ">
-            <label className="w-full text-right mr-2">Firstname:</label>
-            <input type="text" value="Suraj Kumar" className="w-full p-2 border rounded-md" />
-          </div>
-          <div className="flex items-center mb-4">
-            <label className="w-full text-right mr-2">Middlename:</label>
-            <input type="text" value="Lastname" className="w-2/3 p-2 border rounded-md" />
-          </div>
-          <div className="flex items-center mb-4">
-            <label className="w-full text-right mr-2">Lastname:</label>
-            <input type="text" value="Lastname" className="w-full p-2 border rounded-md" />
-          </div>
-          <div className="flex items-center mb-4">
-            <label className="w-full text-right mr-2">E-mail ID:</label>
-            <input type="text" value="Lastname" className="w-2/3 p-2 border rounded-md" />
-          </div>
-          {/* Add more fields similarly */}
-        </div>
-        <div>
-          {/* Add additional fields or buttons */}
-        </div>
-      </form>
+        </form>
+
+      </div>
+
     </div>
   );
 };
